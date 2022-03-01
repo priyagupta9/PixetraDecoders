a35d23231fcd40306179f6d5334b50298099b175
Priya Gupta <priyagupta9u@gmail.com>
2022-03-01 23:51:19 +0530

    otp&ProductPage

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import '../App.css';
+import React from 'react';
+import Otp from "./OtpPage/Otp";
+import Product from "./ProductsPage/Product"
+
+function App() {
+  return (
+    <div>
+    <Otp />
+    <Product />
+    </div>
+  );
+}
+
+export default App;
