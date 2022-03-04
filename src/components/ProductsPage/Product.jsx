dbeaddfe8ffc713ea8e50c9339887aacc3a2facf
Priya Gupta <priyagupta9u@gmail.com>
2022-03-05 00:24:15 +0530

    navbar

--- src/components/ProductsPage/Product.jsx
+++ src/components/ProductsPage/Product.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 import Header from "./Header"
 import Photobook from "./Photobook"
 import  photobookbg from "./img/photobookbg.png"
+import BottomNavbar from "./BottomNavbar"
 
 function Product() {
   return (
     <div className='product-page'>
-    <Header name="Joe" />
-    <Photobook img={photobookbg} />
-    <Photobook img={photobookbg} />
-    <Photobook img={photobookbg} />
-    <Photobook img={photobookbg} />
+      <Header name="Joe" />
+      <Photobook img={photobookbg} />
+      <Photobook img={photobookbg} />
+      <Photobook img={photobookbg} />
+      <Photobook img={photobookbg} />
+      <BottomNavbar />
     </div>
   );
 }
