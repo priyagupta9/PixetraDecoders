af8bc2380f20cbaca5f4ed253e7355352b689d82
Priya Gupta <priyagupta9u@gmail.com>
2022-03-13 20:52:10 +0530

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,16 @@ import '../App.css';
 import React from 'react';
 import Otp from "./OtpPage/Otp";
 import Product from "./ProductsPage/Product"
+import ProductFlow from './ProductFlowP1Page/ProductFlow';
+import Login from "./LoginPage/Login"
 
 function App() {
   return (
     <div>
+    <Login />
     <Otp />
     <Product />
+    <ProductFlow />
     </div>
   );
 }
